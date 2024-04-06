6d8f35774c78e3e7475c5937e0a44c087c0ac811
Gabriel Marques <gabriel.marques@hublocal.com.br>
2024-04-06 02:44:07 -0300

    include groups and assets routes to main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	assets_controller "assets_manager/infra/controller/gin/asset"
+	group_controller "assets_manager/infra/controller/gin/group"
 	user_controller "assets_manager/infra/controller/gin/user"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,8 @@ func main() {
 	r := gin.Default()
 
 	user_controller.SetUserRoutes(r)
+	group_controller.SetGroupRoutes(r)
+	assets_controller.SetAssetRoutes(r)
 
 	r.Run("localhost:8000")
 }
